d2bc4c368359ed1d71331d1c26dfe4d7b9d660b9
Chrille Jansson <chrillejansson@me.com>
2023-12-29 18:42:31 +0100

    Added a validate them all to simplify

--- AddressBook.Core/Models/Contact.cs
+++ AddressBook.Core/Models/Contact.cs
@@ -3,31 +3,36 @@ using System.ComponentModel.DataAnnotations;
 namespace AddressBook.Core.Models;
 
 public class Contact : IRoot
-{ 
+{
     public Contact()
     {
         if (Id == Guid.Empty)
             Id = Guid.NewGuid();
     }
-    
+
     public Guid Id { get; set; }
+
     [Required(ErrorMessage = "First name is required.")]
     [MinLength(2, ErrorMessage = "First name must be at least 2 characters long.")]
     [MaxLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
     public string FirstName { get; set; } = null!;
+
     [Required(ErrorMessage = "Last name is required.")]
     [MinLength(2, ErrorMessage = "Last name must be at least 2 characters long.")]
     [MaxLength(50, ErrorMessage = "Last name cannot be longer than 50 characters.")]
     public string LastName { get; set; } = null!;
+
     [Required(ErrorMessage = "Email is required.")]
     [EmailAddress(ErrorMessage = "Email is not valid.")]
     public string Email { get; set; } = null!;
+
     [Required(ErrorMessage = "Phone number is required.")]
     public string PhoneNumber { get; set; } = null!;
+
     [Required(ErrorMessage = "Address is required.")]
     public Address Address { get; set; } = null!;
-    
-    
+
+
     public string? ValidateFirstname()
     {
         var results = new List<ValidationResult>();
@@ -38,7 +43,7 @@ public class Contact : IRoot
         Validator.TryValidateProperty(FirstName, context, results);
         return results.Select(x => x.ErrorMessage).FirstOrDefault();
     }
-    
+
     public string? ValidateLastname()
     {
         var results = new List<ValidationResult>();
@@ -49,7 +54,7 @@ public class Contact : IRoot
         Validator.TryValidateProperty(LastName, context, results);
         return results.Select(x => x.ErrorMessage).FirstOrDefault();
     }
-    
+
     public string? ValidateEmail()
     {
         var results = new List<ValidationResult>();
@@ -60,7 +65,7 @@ public class Contact : IRoot
         Validator.TryValidateProperty(Email, context, results);
         return results.Select(x => x.ErrorMessage).FirstOrDefault();
     }
-    
+
     public string? ValidatePhoneNumber()
     {
         var results = new List<ValidationResult>();
@@ -70,5 +75,17 @@ public class Contact : IRoot
         };
         Validator.TryValidateProperty(PhoneNumber, context, results);
         return results.Select(x => x.ErrorMessage).FirstOrDefault();
+    }
+
+    public bool Validate()
+    {
+        return ValidateFirstname() == null &&
+               ValidateLastname() == null &&
+               ValidateEmail() == null &&
+               ValidatePhoneNumber() == null &&
+               Address.ValidateStreet() == null &&
+               Address.ValidateCity() == null &&
+               Address.ValidateZipCode() == null &&
+               Address.ValidateCountry() == null;
     }
 }
